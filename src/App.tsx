540ae2e40519c0e88735bf382ebc9dd71c62f7e0
Vladimir Glukhov <glukhov.root@gmail.com>
2024-05-08 00:15:42 +0500

    added tg-web container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { TelegramWebAppContainer } from '@telegram-web-app/core';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const telegram = new TelegramWebAppContainer();
+  telegram.WebApp.ready();
 
   return (
     <>
-      <div>
+      {telegram.WebApp.initData}
+      {/* <p>{tg}</p> */}
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +32,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tg-web-app/'
+  // base: '/tg-web-app/'
 })
