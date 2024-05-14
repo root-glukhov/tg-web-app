1c34aac58cea88f84f7b664c74af7b395ac277d2
Vladimir Glukhov <glukhov.root@gmail.com>
2024-05-14 10:39:51 +0500

    added headers info

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import { TelegramWebAppContainer } from '@telegram-web-app/core';
 
 function App() {
+  const [headers, setHeaders] = useState(null);
+
+  useEffect(() => {
+    fetch('http://78.140.245.95:5001/headers')
+      .then(response => response.json())
+      .then(data => setHeaders(data))
+  }, []);
+
   const telegram = new TelegramWebAppContainer();
   telegram.WebApp.ready();
 
@@ -18,11 +27,20 @@ function App() {
 
   return (
     <>
+      <h3>Headers</h3>
+      {headers ? 
+        <pre className="read-the-docs">
+          {JSON.stringify(headers, null, 2)}
+        </pre>
+      :
+        "Загрузка..."
+      }
+
       <h3>User info</h3>
       <code>telegram.WebApp.initData:</code>
-      <p className="read-the-docs">
-        <pre>{JSON.stringify(jsonObject, null, 2)}</pre>
-      </p>
+      <pre className="read-the-docs">
+        {JSON.stringify(jsonObject, null, 2)}
+      </pre>
       <code>telegram.WebApp.platform: <p className="read-the-docs">{telegram.WebApp.platform}</p></code>
       <code>telegram.WebApp.version: <p className="read-the-docs">{telegram.WebApp.version}</p></code>
     </>
