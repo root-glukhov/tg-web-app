185176b1d0799887dd08918219bb8563324210cd
Vladimir Glukhov <glukhov.root@gmail.com>
2024-05-14 10:55:08 +0500

    edited url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [headers, setHeaders] = useState(null);
 
   useEffect(() => {
-    fetch('http://78.140.245.95:5001/headers')
+    fetch('https://3833-78-140-245-95.ngrok-free.app/headers')
       .then(response => response.json())
       .then(data => setHeaders(data))
   }, []);
